026b9cf99053c7fe7e75fc5130c4d60b609ba04c
Chris Haen <chrishdev2007@gmail.com>
2018-02-20 17:27:27 -0800

    ios stuff

--- ios/src/com/chrishaen/keepitin/IOSLauncher.java
+++ ios/src/com/chrishaen/keepitin/IOSLauncher.java
@@ -2,14 +2,12 @@ package com.chrishaen.keepitin;
 
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
-import com.chrishaen.keepitin.KeepItIn;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
 
 public class IOSLauncher extends IOSApplication.Delegate {
-    @Override
+	
     protected IOSApplication createApplication() {
-    	GADMobileAds.disableSDKCrashReporting();
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
         return new IOSApplication(new KeepItIn(), config);
     }
