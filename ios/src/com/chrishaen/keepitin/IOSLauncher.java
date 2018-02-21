adaaaf1a19b382655a3ed73d20693fd5b14a1d37
Chris Haen <chrishdev2007@gmail.com>
2018-02-20 17:41:09 -0800

    ios

--- ios/src/com/chrishaen/keepitin/IOSLauncher.java
+++ ios/src/com/chrishaen/keepitin/IOSLauncher.java
@@ -4,8 +4,23 @@ import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import org.robovm.apple.coregraphics.CGRect;
+import org.robovm.apple.coregraphics.CGSize;
+import org.robovm.apple.foundation.NSArray;
+import org.robovm.apple.foundation.NSObject;
+import org.robovm.apple.foundation.NSString;
+import org.robovm.apple.uikit.UIScreen;
+import org.robovm.bindings.admob.GADAdSizeManager;
+import org.robovm.bindings.admob.GADBannerView;
+import org.robovm.bindings.admob.GADBannerViewDelegateAdapter;
+import org.robovm.bindings.admob.GADRequest;
+import org.robovm.bindings.admob.GADRequestError;
 
-public class IOSLauncher extends IOSApplication.Delegate {
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication.Delegate;
+import com.badlogic.gdx.utils.Logger;
+
+public class IOSLauncher extends Delegate implements IActivityRequestHandler{
 	
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
